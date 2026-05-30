0176f225718ba0ad52a41e5f03ae595d82da9418
Royhan Antariksa <103055171+royhanantariksaaa@users.noreply.github.com>
2026-05-31 00:52:59 +0700

    Add SolidJS Vite config

---
+++ dashboard/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import solid from "@vitejs/plugin-solid";
+
+export default defineConfig({
+  plugins: [solid()],
+  server: {
+    host: "127.0.0.1",
+    port: 5173,
+  },
+});
